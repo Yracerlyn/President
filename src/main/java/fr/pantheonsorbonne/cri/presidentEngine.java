1435218300843b833af7e106dda61712adc3895d
Yracerlyn <yessicerlyn@gmail.com>
2022-11-11 10:41:39 +0100

    implémenter president engine en essayant de créer pairCombinaison, brelonCombinaison, carreCombinaison

--- src/main/java/fr/pantheonsorbonne/cri/presidentEngine.java
+++ src/main/java/fr/pantheonsorbonne/cri/presidentEngine.java
@@ -1,5 +1,35 @@
 package fr.pantheonsorbonne.cri;
 
 public abstract class presidentEngine {
+
+    protected final Card[] cards;
+    private final static int NBR_PAIRE=2;
+    private final static int NBR_BRELON=2;
+    private final static int NBR_CARREE=2;
+    public boolean pairCombinaison(){
+        
+        for(int i=0; i<this.cards.length;i++){
+            int count =1;
+            for (int j=i+1; j<this.cards.length;j++){
+
+                if(cards[i].getValue()==cards[j].getValue()){
+                    count++;
+                    
+
+            }
+            
+            }
+
+            if (count==NBR_PAIRE){
+                return true;
+            }
+        
+
+        }
+
+        return false;
+    }
+        
+    }
     
-}
+
